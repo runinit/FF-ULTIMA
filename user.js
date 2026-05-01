dd551d01ae2d4913f1352b7e5a949af928afd7e3
Chris D <chris@foosys.ca>
2026-05-01 11:18:29 -0400

    refactor(sidebery): collapse indent prefs into mode

--- user.js
+++ user.js
@@ -79,8 +79,7 @@ user_pref("ultima.sidebar.revamped.hide.when.horizontal", true);
 /* sidebery */
 user_pref("ultima.sidebery.autohide", true);
 user_pref("ultima.sidebery.expandon.inactive.windows", false);
-user_pref("ultima.sidebery.indent.compact", false); // compact tree indentation
-user_pref("ultima.sidebery.indent.standard", true); // default Sidebery indentation
+user_pref("ultima.sidebery.indent.mode", "standard"); // compact or standard
 user_pref("user.theme.xtension.sidebery", true);
 
 /* findbar */
