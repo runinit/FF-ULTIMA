aa0217cbce971866886a6d08ad4d6fa4bf1b4340
Chris D <chris@foosys.ca>
2026-05-01 11:17:07 -0400

    refactor(sidebery): annotate indent prefs

--- user.js
+++ user.js
@@ -79,8 +79,8 @@ user_pref("ultima.sidebar.revamped.hide.when.horizontal", true);
 /* sidebery */
 user_pref("ultima.sidebery.autohide", true);
 user_pref("ultima.sidebery.expandon.inactive.windows", false);
-user_pref("ultima.sidebery.indent.compact", false);
-user_pref("ultima.sidebery.indent.standard", true);
+user_pref("ultima.sidebery.indent.compact", false); // compact tree indentation
+user_pref("ultima.sidebery.indent.standard", true); // default Sidebery indentation
 user_pref("user.theme.xtension.sidebery", true);
 
 /* findbar */
